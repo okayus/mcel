10eb3ac38e35a5b6ca08337df18ee1bec398a883
okayus <toshiaki.mukai.9981@gmail.com>
2024-01-10 09:05:46 +0900

    add:HeadeMenu.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
+      <header-menu />
+    </div>
+    <div>
       <table-component />
     </div>
 </template>
 
 <script lang="ts" setup>
 import TableComponent from './components/TableComponent.vue';
+import HeaderMenu from './components/HeaderMenu.vue';
 </script>
 
 <style scoped>
---
+++ src/components/HeaderMenu.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { saveAs } from 'file-saver';
+
+const fileName = ref('sample.txt');
+
+const downloadFile = () => {
+  const blob = new Blob(['Hello, world!'], { type: 'text/plain' });
+  saveAs(blob, fileName.value);
+};
+
+</script>
+
+<template>
+  <div>
+    <input type="text" v-model="fileName" />
+    <button @click="downloadFile">download</button>
+  </div>
+</template>
+
+<style scoped></style>
