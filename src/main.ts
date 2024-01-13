f2e542016edf137423ad24fc405481f73cd00179
okayus <toshiaki.mukai.9981@gmail.com>
2024-01-13 16:20:11 +0900

    core:tailwind適用

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import '@imengyu/vue3-context-menu/lib/vue3-context-menu.css'
 import ContextMenu from '@imengyu/vue3-context-menu'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import './index.css'
 
 const app = createApp(App);
 app.use(ContextMenu)  
